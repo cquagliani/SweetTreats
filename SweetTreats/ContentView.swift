b40a9feae0a3ac28e4acc3b1cf63b8e9605fcd09
Chris Quagliani <chrisquag24@berkeley.edu>
2023-11-06 18:45:11 -0800

    Initial Commit

---
+++ SweetTreats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SweetTreats
+//
+//  Created by Chris Quagliani on 11/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SweetTreats/SweetTreatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetTreatsApp.swift
+//  SweetTreats
+//
+//  Created by Chris Quagliani on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SweetTreatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
