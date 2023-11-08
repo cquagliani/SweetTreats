a99c9ad75e13a2c318481416689aeea455ece707
Chris Quagliani <chrisquag24@berkeley.edu>
2023-11-08 12:37:09 -0800

    Add image background to DessertCard

--- SweetTreats/DESSERTS/DessertCard.swift
+++ SweetTreats/DESSERTS/DessertCard.swift
@@ -11,14 +11,31 @@ struct DessertCard: View {
     let dessert: Dessert
     
     var body: some View {
-        HStack(alignment: .top) {
-            URLImage(urlString: dessert.strMealThumb, width: 100, height: 100)
-                .clipShape(RoundedRectangle(cornerRadius: 16))
-            Text(dessert.strMeal)
-                .bold()
-                .font(.system(size: 18))
-                .padding(5)
+        ZStack {
+            URLImage(urlString: dessert.strMealThumb, width: 350, height: 100)
+                .cornerRadius(25)
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                .clipped()
+            
+            // Dark overlay to improve title readability
+            Rectangle()
+                .frame(width: 350, height: 100)
+                .foregroundColor(.black)
+                .opacity(0.3)
+                .cornerRadius(25)
+                .shadow(radius: 10)
+            
+            HStack {
+                Text(dessert.strMeal)
+                    .font(
+                        .title2
+                        .bold()
+                    )
+                    .foregroundColor(.white)
+                    .padding(.leading)
+                Spacer() // Aligns text to the left
             }
-            .padding(5)
         }
+        .cornerRadius(25) // Must match the image's corner radius
     }
+}
