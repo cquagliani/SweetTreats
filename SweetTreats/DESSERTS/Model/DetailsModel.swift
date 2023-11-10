5ebeb74c18171e72d187d367750cbc84b07a315e
Chris Quagliani <chrisquag24@berkeley.edu>
2023-11-10 11:38:53 -0800

    Add previously missing properties to accomodate maximum ingredients and measurements. Organize ingredients and measurements into arrays.

--- SweetTreats/DESSERTS/Model/DetailsModel.swift
+++ SweetTreats/DESSERTS/Model/DetailsModel.swift
@@ -16,6 +16,7 @@ struct DessertDetail: Hashable, Codable {
     let strMealThumb: String
     let idMeal: String
     let strCategory: String
+    
     let strInstructions: String
     let strIngredient1: String
     let strIngredient2: String
@@ -27,6 +28,17 @@ struct DessertDetail: Hashable, Codable {
     let strIngredient8: String
     let strIngredient9: String
     let strIngredient10: String
+    let strIngredient11: String
+    let strIngredient12: String
+    let strIngredient13: String
+    let strIngredient14: String
+    let strIngredient15: String
+    let strIngredient16: String
+    let strIngredient17: String
+    let strIngredient18: String
+    let strIngredient19: String
+    let strIngredient20: String
+    
     let strMeasure1: String
     let strMeasure2: String
     let strMeasure3: String
@@ -37,4 +49,62 @@ struct DessertDetail: Hashable, Codable {
     let strMeasure8: String
     let strMeasure9: String
     let strMeasure10: String
+    let strMeasure11: String
+    let strMeasure12: String
+    let strMeasure13: String
+    let strMeasure14: String
+    let strMeasure15: String
+    let strMeasure16: String
+    let strMeasure17: String
+    let strMeasure18: String
+    let strMeasure19: String
+    let strMeasure20: String
+    
+    var ingredients: [String] { 
+        [strIngredient1, 
+         strIngredient2,
+         strIngredient3,
+         strIngredient4,
+         strIngredient5,
+         strIngredient6,
+         strIngredient7,
+         strIngredient8,
+         strIngredient9,
+         strIngredient10,
+         strIngredient11,
+         strIngredient12,
+         strIngredient13,
+         strIngredient14,
+         strIngredient15,
+         strIngredient16,
+         strIngredient17,
+         strIngredient18,
+         strIngredient19,
+         strIngredient20,
+        ].filter { !$0.isEmpty }
+    }
+    
+    var measurements: [String] {
+        [strMeasure1, 
+         strMeasure2,
+         strMeasure3,
+         strMeasure4,
+         strMeasure5,
+         strMeasure6,
+         strMeasure7,
+         strMeasure8,
+         strMeasure9,
+         strMeasure10,
+         strMeasure11,
+         strMeasure12,
+         strMeasure13,
+         strMeasure14,
+         strMeasure15,
+         strMeasure16,
+         strMeasure17,
+         strMeasure18,
+         strMeasure19,
+         strMeasure20
+        ].filter { !$0.isEmpty }
+    }
 }
