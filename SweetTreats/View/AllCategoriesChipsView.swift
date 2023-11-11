abab3ea7cd97b091b83481493c14f36bc7ee68b3
Chris Quagliani <chrisquag24@berkeley.edu>
2023-11-10 22:08:21 -0800

    Create filter chips for the home page redesign.

---
+++ SweetTreats/View/AllCategoriesChipsView.swift
@@ -0,0 +1,8 @@
+//
+//  AllCategoriesChipsView.swift
+//  SweetTreats
+//
+//  Created by Chris Quagliani on 11/10/23.
+//
+
+import Foundation
---
+++ SweetTreats/View/HomeFeedView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeFeedView.swift
+//  SweetTreats
+//
+//  Created by Chris Quagliani on 11/10/23.
+//
+
+import SwiftUI
+
+struct HomeFeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeFeedView()
+}
